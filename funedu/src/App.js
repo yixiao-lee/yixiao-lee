311b3dee15ee839d827b413e2c04489ea2bdb8d2
liyixiao <yixiao.lee@icloud.com>
2022-02-22 23:03:02 +0800

    init

---
+++ funedu/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <main>
+
+      </main>
+      <Footer>
+
+      </Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ funedu/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <span>FunEdu</span>
+            <ul>
+                <li>Login | Register</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
