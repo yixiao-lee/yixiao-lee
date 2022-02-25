eed4f6be269c80c8d70bdc31bea089f53ba9c8c9
liyixiao <yixiao.lee@icloud.com>
2022-02-25 23:18:45 +0800

    add store and using mui

--- funedu/src/App.js
+++ funedu/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
 function App() {
   return (
-    <div className="app">
+    <div className="container">
       <Header></Header>
       <Main>main content</Main>
       <Footer>Footer</Footer>
--- funedu/src/components/Header.js
+++ funedu/src/components/Header.js
@@ -1,26 +1,79 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
-// import { AiOutlineBars } from "react-icons/ai";
-// import { RiCloseLine } from "react-icons/ri";
 import { SiKhanacademy } from "react-icons/si";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import MenuItem from '@mui/material/MenuItem';
+import { useSelector, useDispatch } from 'react-redux'
+import { populateUser } from "../slices/userSlice";
 
 const Header = () => {
-  // const [showMenu, setShowMenu] = useState(false);
 
-  // const toggleMenu = () => {
-  //   setShowMenu(!showMenu);
-  // };
+  const user = useSelector((state) => state.userReducer.value)
+  const dispatch = useDispatch()
+  const settings = ['Profile', 'Logout'];
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className="header">
-      <div className="header_left">
-       <SiKhanacademy size = "38" color = "cyan" />
-       <div className="header_left_text">Fun <span className="header_left_char">E</span>ducation</div>
-      </div>
-      <div className="header_right">
-        <img className="rounded-circle" src="https://picsum.photos/id/3/80/80"/>
-      </div>
-    </div>
+      <AppBar position="static">
+        <Container>
+          <Toolbar disableGutters>
+            <Box component="div" sx={{ flexGrow: 1, display: "flex", ml:1 }}>
+              <SiKhanacademy size = "38" color = "orangered" />
+              <Typography
+                variant="h5"
+                noWrap
+                sx = {{ml:1, fontWeight: 500}}
+              >
+                Fun <span className="header_e_char">E</span>ducation
+              </Typography>
+            </Box>  
+            <Box sx={{ flexGrow: 0 }}>
+              {/* <Tooltip title="Open settings"> */}
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="https://picsum.photos/id/3/80/80" />
+                </IconButton>
+              {/* </Tooltip> */}
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
   );
 };
 
